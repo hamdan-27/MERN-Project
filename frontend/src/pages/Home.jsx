853828cffd8ef7fe62fd1689050b0fa333c15709
hamdan-27 <hamdan102703@gmail.com>
2024-02-22 23:46:58 +0400

    working

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
 import { AiOutlineEdit } from 'react-icons/ai';
 import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineDelete, MdOutlineAddBox } from 'react-icons/md';
+import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
 
 const Home = () => {
     const [books, setBooks] = useState([]);
     const [loading, setLoading] = useState(false);
+
     useEffect(() => {
         setLoading(true);
         axios
@@ -21,7 +22,7 @@ const Home = () => {
                 console.log(error);
                 setLoading(false);
             })
-    })
+    }, []);
     return (
         <div className='p-4'>
             <div className='flex justify-between items-center'>
