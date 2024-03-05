4ea3808132ad76d2926a5d459139b50a53269ab1
hamdan-27 <hamdan102703@gmail.com>
2024-03-05 15:22:53 +0400

    hide mongo url

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,5 @@
 export const PORT = 5555;
 
 export const mongoDBURL = 
-"mongodb+srv://root:root@bookstore-mern.pltxz4p.mongodb.net/books-collection?retryWrites=true&w=majority&appName=BookStore-MERN";
+"{YOUR_MONGO_URL}";
 
